db8c4b27ac0c449f1fc22c530d5027c15e76cb92
Elemma00 <faundez76@gmail.com>
2024-11-18 13:07:38 -0300

    end invoice app

--- 2-invoice-app/src/app/components/invoice/invoice.component.ts
+++ 2-invoice-app/src/app/components/invoice/invoice.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { InvoiceService } from '../../services/invoice.service';
 import { Invoice } from '../../models/invoice';
-import { InvoiceViewComponent } from '../invoice-view/invoice-view.component';
+import { InvoiceService } from '../../services/invoice.service';
 import { ClientViewComponent } from '../client-view/client-view.component';
 import { CompanyViewComponent } from '../company-view/company-view.component';
+import { InvoiceViewComponent } from '../invoice-view/invoice-view.component';
 import { ListItemsComponent } from '../list-items/list-items.component';
-import { RowItemComponent } from '../row-item/row-item.component';
+import { TotalComponent } from '../total/total.component';
+import { FormItemComponent } from '../form-item/form-item.component';
+import { Item } from '../../models/item';
 
 @Component({
   selector: 'app-invoice',
   standalone: true,
-  imports: [InvoiceViewComponent, ClientViewComponent, CompanyViewComponent, ListItemsComponent],
+  imports: [InvoiceViewComponent,
+    ClientViewComponent,
+    CompanyViewComponent,
+    ListItemsComponent,
+    TotalComponent,
+    FormItemComponent],
   templateUrl: './invoice.component.html',
 })
 export class InvoiceComponent implements OnInit {
@@ -21,5 +28,13 @@ export class InvoiceComponent implements OnInit {
 
   ngOnInit(): void {
     this.invoice = this.service.getInvoice();
+  }
+
+  removeItem(id: number) {
+    this.invoice = this.service.remove(id);
+  }
+
+  addItem(item: Item) {
+    this.invoice = this.service.add(item);
   }
 }
--- 2-invoice-app/src/app/services/invoice.service.ts
+++ 2-invoice-app/src/app/services/invoice.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Invoice } from '../models/invoice';
 import { invoiceData } from '../data/invoice.data';
+import { Item } from '../models/item';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,29 @@ export class InvoiceService {
   constructor() { }
 
   getInvoice(): Invoice {
-    return this.invoice;
+    const total = this.calculateTotal();
+    // Se usa spread operator para copiar el objeto invoice y agregarle la propiedad total
+    return { ... this.invoice, total: total };
+  }
+
+  remove(id: number): Invoice {
+    this.invoice.items = this.invoice.items.filter(item => item.id != id);
+    const total = this.calculateTotal();
+    return { ... this.invoice, total: total };
+  }
+
+  add(item:Item): Invoice {
+    this.invoice.items = [... this.invoice.items, item];
+    const total = this.calculateTotal();
+    return { ... this.invoice, total: total };
+  }
+
+  calculateTotal() {
+    // let total = 0;
+    // this.invoice.items.forEach(item => {
+    //   total += item.total();
+    // });
+    // return total;
+    return this.invoice.items.reduce((acc, item) => acc + (item.price * item.quantity), 0);
   }
 }
