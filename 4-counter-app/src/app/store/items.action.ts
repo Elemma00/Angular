3c71875360a483fba8c2c99d705fe4a7e9d999bb
Elemma00 <faundez76@gmail.com>
2024-12-24 18:37:08 -0300

    add: ngrx store installation

--- 4-counter-app/src/app/store/items.action.ts
+++ 4-counter-app/src/app/store/items.action.ts
@@ -1,5 +1,13 @@
-import { createAction } from "@ngrx/store";
+import { createAction, props } from "@ngrx/store";
 
-export const increment = createAction('[Counter Component] Increment');
+/**
+ * Las acciones llaman a los reducers para que se actualice el estado de la aplicación
+ * Las acciones son objetos simples que tienen una propiedad type que identifica la acción que se va a realizar
+ * Las acciones pueden tener una propiedad payload que contiene los datos necesarios para realizar la acción, por ejemplo
+ * la propiedad add en la acción increment que representa la cantidad que se va a sumar al contador
+ * 
+ */
+
+export const increment = createAction('[Counter Component] Increment', props<{ add: number }>());
 export const decrement = createAction('[Counter Component] Decrement');
 export const reset = createAction('[Counter Component] Reset');
--- 4-counter-app/src/app/store/items.reducer.ts
+++ 4-counter-app/src/app/store/items.reducer.ts
@@ -3,9 +3,14 @@ import { decrement, increment, reset } from "./items.action";
 
 export const initialState = 0;
 
+/**
+ * Reducer que se encarga de actualizar el estado de la aplicación
+ * @param state Estado actual de la aplicación
+ */
+
 export const counterReducer = createReducer(
     initialState,
-    on(increment, (state) =>  state + 1),
+    on(increment, (state, { add }) =>  state + add),
     on(decrement, (state) => state - 1),
     on(reset, (state) => 0)
 )
